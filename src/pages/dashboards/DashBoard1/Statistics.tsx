f7313d5dd9b1b1521cec28db13f39b2d51a021f9
eraykpln <1eraykaplan@gmail.com>
2024-09-27 17:03:17 +0300

    Update .gitignore to exclude node_modules and yarn.lock

--- src/pages/dashboards/DashBoard1/Statistics.tsx
+++ src/pages/dashboards/DashBoard1/Statistics.tsx
@@ -1,54 +1,54 @@
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row } from "react-bootstrap";
 
 // component
-import StatisticsWidget1 from '../../../components/StatisticsWidget1';
-import StatisticsWidget2 from '../../../components/StatisticsWidget2';
+import StatisticsWidget1 from "../../../components/StatisticsWidget1";
+import StatisticsWidget2 from "../../../components/StatisticsWidget2";
 
 const Statistics = () => {
-    return (
-        <Row>
-            <Col xl={3} md={6}>
-                <StatisticsWidget1
-                    title="Total Revenue"
-                    data={58}
-                    stats={256}
-                    color={'#f05050'}
-                    subTitle="Revenue today"
-                />
-            </Col>
-            <Col xl={3} md={6}>
-                <StatisticsWidget2
-                    variant="success"
-                    title="Sales Analytics"
-                    trendValue="32%"
-                    trendIcon="mdi mdi-trending-up"
-                    stats={8451}
-                    subTitle="Revenue today"
-                    progress={77}
-                />
-            </Col>
-            <Col xl={3} md={6}>
-                <StatisticsWidget1
-                    title="Statistics"
-                    color={'#ffbd4a'}
-                    data={80}
-                    stats={4569}
-                    subTitle="Revenue today"
-                />
-            </Col>
-            <Col xl={3} md={6}>
-                <StatisticsWidget2
-                    variant="pink"
-                    title="Daily Sales"
-                    trendValue="32%"
-                    trendIcon="mdi mdi-trending-up"
-                    stats={158}
-                    subTitle="Revenue today"
-                    progress={77}
-                />
-            </Col>
-        </Row>
-    );
+  return (
+    <Row>
+      <Col xl={3} md={6}>
+        <StatisticsWidget1
+          title="Aktif Tur"
+          data={58}
+          stats={256}
+          color={"#f05050"}
+          subTitle="Aktif tur"
+        />
+      </Col>
+      <Col xl={3} md={6}>
+        <StatisticsWidget2
+          variant="success"
+          title="Satış Sayısı"
+          trendValue="32%"
+          trendIcon="mdi mdi-trending-up"
+          stats={8451}
+          subTitle="Günlük Satış"
+          progress={77}
+        />
+      </Col>
+      <Col xl={3} md={6}>
+        <StatisticsWidget1
+          title="Yarın Başlayacak Tur"
+          color={"#ffbd4a"}
+          data={80}
+          stats={4569}
+          subTitle="Yarın Başlayacak"
+        />
+      </Col>
+      <Col xl={3} md={6}>
+        <StatisticsWidget2
+          variant="pink"
+          title="Yarın Tamamlanacak"
+          trendValue="32%"
+          trendIcon="mdi mdi-trending-up"
+          stats={158}
+          subTitle="Yarın Tamamlanacak"
+          progress={77}
+        />
+      </Col>
+    </Row>
+  );
 };
 
 export default Statistics;
